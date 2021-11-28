27dc32986baf7e142de50b93f0926cc3c7c655fc
Le Minh Khoi <minhkhoile2001nsg@gmail.com>
2021-11-28 20:06:20 +0700

    Update train.sh

--- train.sh
+++ train.sh
@@ -23,7 +23,6 @@ python train.py $DATAPATH \
 -a $ARCH --optimizer adam --lr 0.0005 -s $src -t $tgt --label-smoothing 0.1 \
 --dropout 0.3 --max-tokens 4000 --min-lr '1e-09' --lr-scheduler inverse_sqrt --weight-decay 0.0001 \
 --criterion label_smoothed_cross_entropy --max-update 150000 --warmup-updates 4000 --warmup-init-lr '1e-07' \
---no-epoch-checkpoints \
 --adam-betas '(0.9,0.98)' --save-dir $SAVEDIR --share-all-embeddings $warmup \
 --encoder-bert-dropout --encoder-bert-dropout-ratio $bedropout \
 --bert-model-name vinai/phobert-base | tee -a $SAVEDIR/training.log
